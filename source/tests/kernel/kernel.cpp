4ac4e96bed7ff1d981ba0de7004362d2639bd1a0
Subv <subv2112@gmail.com>
2015-12-27 19:04:32 -0500

    Tests/Kernel: Added hwtests for svcArbitrateAddress

--- source/tests/kernel/kernel.cpp
+++ source/tests/kernel/kernel.cpp
@@ -4,10 +4,12 @@ namespace Kernel {
 
 namespace Ports { void TestAll(); }
 namespace WaitSynch { void TestAll(); }
+namespace AddressArbiter { void TestAll(); }
 
 void TestAll() {
     Ports::TestAll();
     WaitSynch::TestAll();
+    AddressArbiter::TestAll();
 }
 
 } // namespace
